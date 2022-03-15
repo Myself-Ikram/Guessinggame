9f774c409cef48d409b68c6fbe96b7f7877be0d9
Myself-Ikram <97652328+Myself-Ikram@users.noreply.github.com>
2022-03-15 16:28:13 +0530

    Game uploaded

---
+++ guessinggame.py
@@ -0,0 +1,15 @@
+input("Select a number and multiply by 3")
+eo = input("Type e for even and o for odd: ")
+if eo=='e':
+  print("Divide by 2")
+else:
+  print("Add 1 and divide by 2")
+d = input("Multiply the resultant the number by 3 and ENTER if done")
+result = int(input("DIVIDE THE NUMBER WITH 9 AND TELL QUOTENT: "))
+if eo == 'e':
+  x = result*2
+  print(x)
+else:
+  x = result*2+1
+  print(x)
+
